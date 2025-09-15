d5fee791b60e4aba2f1cd58a13c9d6173081d73c
HrabarDzmitry <hrabardzmitry@gmail.com>
2025-09-15 18:42:48 +0300

    Refactor: combine tableExists condition and implement safeDown method

--- src/migrations/m250914_120000_rename_plugin_tables.php
+++ src/migrations/m250914_120000_rename_plugin_tables.php
@@ -6,22 +6,21 @@ use craft\db\Migration;
 
 class m250914_120000_rename_plugin_tables extends Migration
 {
+    private array $tables = [
+        'alt-text-lab_asset' => 'alttextlab_asset',
+        'alt-text-lab_log' => 'alttextlab_log',
+        'alt-text-lab_bulk_generation' => 'alttextlab_bulk_generation',
+    ];
+
     public function safeUp(): bool
     {
-        $tables = [
-            'alt-text-lab_asset' => 'alttextlab_asset',
-            'alt-text-lab_log' => 'alttextlab_log',
-            'alt-text-lab_bulk_generation' => 'alttextlab_bulk_generation',
-        ];
 
-        foreach ($tables as $oldName => $newName) {
+        foreach ($this->tables as $oldName => $newName) {
             $old = Craft::$app->db->tablePrefix . $oldName;
             $new = Craft::$app->db->tablePrefix . $newName;
 
-            if ($this->db->tableExists($old)) {
-                if (!$this->db->tableExists($new)) {
-                    $this->renameTable($old, $new);
-                }
+            if ($this->db->tableExists($old) && !$this->db->tableExists($new)) {
+                $this->renameTable($old, $new);
             }
         }
 
@@ -30,7 +29,15 @@ class m250914_120000_rename_plugin_tables extends Migration
 
     public function safeDown(): bool
     {
-        return false;
+        foreach ($this->tables as $oldName => $newName) {
+            $old = Craft::$app->db->tablePrefix . $oldName;
+            $new = Craft::$app->db->tablePrefix . $newName;
+
+            if ($this->db->tableExists($new) && !$this->db->tableExists($old)) {
+                $this->renameTable($new, $old);
+            }
+        }
+        return true;
     }
 
 }
